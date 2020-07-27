c39109528cd5f1ad5dfbdff6ad7e1316af4dc4d5
julian2358 <julianreeves2358@gmail.com>
2020-07-27 12:44:41 -0400

    final api push- added sql server

--- src/public/index.js
+++ src/public/index.js
@@ -1,12 +1,29 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.password,
+    database: 'todo' 
+   });
+
+   try {
+    connection.connect();
+   } catch (e) {
+    console.log('Oops. Connection to MySQL failed.');
+    console.log(e);
+   }
+
 //port listening
 const api = express();
 api.use(express.static(__dirname));
 api.use(bodyParser.json());
 console.log(__dirname) 
 
-api.listen(4000, () => {
+api.listen(2000, () => {
   console.log('API up and running!');
 });
 
@@ -14,12 +31,12 @@ api.listen(4000, () => {
 
 api.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('It works!');
+    
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+     if (error) return res.json({ error: error });
+   connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+      if (error) return res.json({ error: error });
+   console.log(results[0]['LAST_INSERT_ID']);
+     });
+    });
    });
-
-
-// api.get('/', (req, res) => {
-//     console.log(req);
-//     res.send('Hello, world!');
-//   });
-
