edee0148ce65bcca15e875315283c75a44c9fb3d
julian2358 <julianreeves2358@gmail.com>
2020-07-27 12:05:26 -0400

    serve todo list app

--- src/public/index.js
+++ src/public/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 //port listening
 const api = express();
+api.use(express.static(__dirname));
+console.log(__dirname) 
+
 api.listen(4000, () => {
   console.log('API up and running!');
 });
 
 // routes
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello, world!');
-  });
+// api.get('/', (req, res) => {
+//     console.log(req);
+//     res.send('Hello, world!');
+//   });
+
