f575e3c19f84d6b38d4faa397d595b851e753f06
julian2358 <julianreeves2358@gmail.com>
2020-07-27 12:15:30 -0400

    connected todo input to api

--- src/public/index.js
+++ src/public/index.js
@@ -1,14 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 //port listening
 const api = express();
 api.use(express.static(__dirname));
+api.use(bodyParser.json());
 console.log(__dirname) 
 
 api.listen(4000, () => {
   console.log('API up and running!');
 });
 
-// routes
+//routes
+
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('It works!');
+   });
+
+
 // api.get('/', (req, res) => {
 //     console.log(req);
 //     res.send('Hello, world!');
